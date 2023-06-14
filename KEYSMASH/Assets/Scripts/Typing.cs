3e953273b188de4a0e5eedbe13fe504761596f53
Ayaan <53174223+astro648@users.noreply.github.com>
2023-06-14 11:37:22 -0700

    variable

---
+++ KEYSMASH/Assets/Scripts/Typing.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Typing : MonoBehaviour
+{
+    // need word bank
+    public Text wordOutput = null;
+    private string remainingWord = string.Empty;
+    private string currentWord = string.Empty;
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
