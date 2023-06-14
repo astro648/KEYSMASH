1383fc66582ae69b58c552a926a6bcd0d3e5b576
Ayaan <53174223+astro648@users.noreply.github.com>
2023-06-14 11:40:52 -0700

    functions

--- KEYSMASH/Assets/Scripts/Typing.cs
+++ KEYSMASH/Assets/Scripts/Typing.cs
@@ -8,15 +8,52 @@ public class Typing : MonoBehaviour
     // need word bank
     public Text wordOutput = null;
     private string remainingWord = string.Empty;
-    private string currentWord = string.Empty;
+    private string currentWord = "trojan"; // placeholder
     
     private void Start()
     {
-        
+        SetCurrentWord();
+    }
+
+    private void SetCurrentWord()
+    {
+        // get word from word bank
+        SetRemainingWord(currentWord);
+    }
+
+    private void SetRemainingWord(string NewString)
+    {
+        remainingWord = NewString;
+        wordOutput.text = remainingWord;
     }
 
     private void Update()
     {
         
+    }
+
+    private void CheckInput()
+    {
+
+    }
+
+    private void EnterLetter(string typedLetter)
+    {
+
+    }
+
+    private bool IsCorrectLetter(string letter)
+    {
+        return false;
+    }
+
+    private void RemoveLetter()
+    {
+
+    }
+
+    private bool IsWordComplete()
+    {
+
     }
 }
